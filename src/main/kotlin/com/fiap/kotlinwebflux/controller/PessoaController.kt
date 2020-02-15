f6d90872f16882fd6bfc5c36af1b97730cf97006
Jaqueline.Laurenti <jaquelinelaurenti@gmail.com>
2020-02-15 17:48:32 -0300

    Implementacao da Programacao Reativa no WebFlux

--- src/main/kotlin/com/fiap/kotlinwebflux/controller/PessoaController.kt
+++ src/main/kotlin/com/fiap/kotlinwebflux/controller/PessoaController.kt
@@ -1,19 +1,37 @@
 package com.fiap.kotlinwebflux.controller
 
 import com.fiap.kotlinwebflux.dto.PessoaDTO
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
+import java.awt.PageAttributes
+import java.time.Duration
 
 @RestController
 class PessoaController {
 
-    @GetMapping
+    /*@GetMapping
     fun getPessoas(): Flux<PessoaDTO>  = Flux.create{ emitter->
         emitter.next(PessoaDTO(1, "Jaqueline", "12345664"))
         emitter.next(PessoaDTO(1, "Joao", "12345664"))
         emitter.complete()
-    }
+    }*/
+
+    // Neste exemplo o banco retorna de forma reativa
+    // a medida que ele vai completando ele vai mostrando
+    @GetMapping(produces = [MediaType.APPLICATION_STREAM_JSON_VALUE])
+    fun getPessoas(): Flux<PessoaDTO>  = Flux.just(
+        arrayListOf(
+                PessoaDTO(1,"Jaque", "1234566"),
+                PessoaDTO(1,"Jaque", "1234566"),
+                PessoaDTO(1,"Jaque", "1234566")
+        )
+    )
+            .flatMapIterable { it }
+            .delayElements(Duration.ofSeconds(2))
+
+
 }
 
 // Kotlin assim como java é uma linguagem fortamente tipada
